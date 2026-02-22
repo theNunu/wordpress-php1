b7665833a161b752303e977f39a819d7060b093f
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-02-22 17:55:59 -0500

    8 commit

--- mi-api.php
+++ mi-api.php
@@ -3,6 +3,7 @@
 require_once plugin_dir_path(__FILE__) . 'services/SignService.php';
 require_once plugin_dir_path(__FILE__) . 'services/Saludar.php';
 require_once plugin_dir_path(__FILE__) . 'services/PersonalizedGreetingService.php';
+require_once plugin_dir_path(__FILE__) . 'services/Sign.php';
 
 add_action('wp_ajax_nopriv_dpti_create_sign', 'dpti_create_sign');
 add_action('wp_ajax_dpti_create_sign', 'dpti_create_sign');
@@ -138,3 +139,46 @@ function greet_someone2()
 
 }
 //http://localhost/miwp/wp-admin/admin-ajax.php  -> LA RUTA POR DEFECTO A USAR EN POSTMAN
+
+add_action('wp_ajax_nopriv_dpti_make_sign', 'dpti_make_sign');
+add_action('wp_ajax_dpti_make_sign', 'dpti_make_sign');
+
+function dpti_make_sign()
+{
+    echo '<pre>'; var_dump( 'wazaaaaaaaaaaaa'); echo '</pre>'; die();
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        wp_send_json([
+            "status" => "error",
+            "msg" => "Metodo no permitido"
+        ], 405);
+    }
+
+    // Validar obligatorios
+    $required = ['firstName', 'lastName', 'age'];
+
+    foreach ($required as $r) {
+        if (!isset($_POST[$r]) || empty($_POST[$r])) {
+            wp_send_json([
+                "status" => "error",
+                "msg" => "El campo {$r} es obligatorio"
+            ], 400);
+        }
+    }
+
+    // Sanitizar
+    $data = [
+        'firstName' => sanitize_text_field($_POST['firstName']),
+        'lastName' => sanitize_text_field($_POST['lastName']),
+        'age' => intval($_POST['age']),
+        'phoneNumber' => sanitize_text_field($_POST['phoneNumber'] ?? '')
+    ];
+
+    // // Si viene archivo lo tomamos, si no null
+    // $file = $_FILES['pdfFile'] ?? null;
+
+    $response = Sign::createSign($data);
+
+    wp_send_json($response);
+
+}
